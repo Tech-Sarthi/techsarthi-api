0f39c0bfa1049ad26dae8ecad5eb9e62c624fdea
Alazord <Alazord@users.noreply.github.com>
2022-10-13 03:38:59 +0530

    pop-up added and carousel made

--- models/project.js
+++ models/project.js
@@ -1,74 +1,82 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-const projectSchema=new mongoose.Schema({
-    name_of_industry:{
-        type:String,
-        required:true,
-        index:true,
-        text:true,
+const projectSchema = new mongoose.Schema(
+  {
+    name_of_industry: {
+      type: String,
+      required: true,
+      index: true,
+      text: true,
     },
-    type_of_industry:{
-        type:String,
-        required:true,
-        index:true,
-        text:true,
+    type_of_industry: {
+      type: String,
+      required: true,
+      index: true,
+      text: true,
     },
-    website:{
-        type:String,
-        index:true,
-        text:true,
+    website: {
+      type: String,
+      index: true,
+      text: true,
     },
-    name_of_contact_person:{
-        type:String,
-        required:true,
-        index:true,
-        text:true,
+    name_of_contact_person: {
+      type: String,
+      required: true,
+      index: true,
+      text: true,
     },
-    designation_of_contact_person:{
-        type:String,
-        required:true,
-        index:true,
-        text:true,
+    designation_of_contact_person: {
+      type: String,
+      required: true,
+      index: true,
+      text: true,
     },
-    email_of_contact_person:{
-        type:String,
-        required:true,
-        index:true,
-        text:true,
+    email_of_contact_person: {
+      type: String,
+      required: true,
+      index: true,
+      text: true,
     },
-    problem_faced:{
-        type:String,
-        required:true,
-        index:true,
-        text:true,
+    problem_faced: {
+      type: String,
+      required: true,
+      index: true,
+      text: true,
     },
-    other_details:{
-        type:String,
-        index:true,
-        text:true,
+    other_details: {
+      type: String,
+      index: true,
+      text: true,
     },
-    industry_logo:{
-        type:String,
+    expected_outcome: {
+      type: String,
+      index: true,
+      text: true,
     },
-    problem_details:{
-        type:String,
+    industry_logo: {
+      type: String,
+    },
+    problem_details: {
+      type: String,
     },
 
     likeCount: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
     // tags:[{
     //     type:String,
     //     text:true,
     //     maxlength:20,
     // }],
-    verified:{
-        type: Boolean,
-        default: false
-    }
-},{timestamps:true})
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-const Project=mongoose.model('Project',projectSchema);
+const Project = mongoose.model("Project", projectSchema);
 
-module.exports=Project;
+module.exports = Project;
