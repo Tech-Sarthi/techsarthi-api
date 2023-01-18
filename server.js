ca10ef91922428f00bc2fc97a90c55e1bc82783c
aayu1401 <aayushi_m@me.iitr.ac.in>
2023-01-18 17:20:31 +0530

    deploy

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ fs.readdirSync(path.join(__dirname, 'routes'), { withFileTypes: true })
   .filter((dirent) => dirent.isFile())
   .map((dirent) => dirent.name)
   .map((r) => {
-    app.use("/", require("./routes/" + r));
+    app.use("/api", require("./routes/" + r));
   });
 
 // port
